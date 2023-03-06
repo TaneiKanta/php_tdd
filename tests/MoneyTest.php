418682834da839d1215ad92f7cc94b4bf1d42742
TaneiKanta <hetaleotu@gmail.com>
2023-03-06 15:03:10 +0900

    add: Dollerクラス

--- tests/MoneyTest.php
+++ tests/MoneyTest.php
@@ -1,17 +1,15 @@
 <?php
 declare(strict_types = 1);
 
-namespace Taneikanta\PhpTdd\Money;
-
 use PHPUnit\Framework\TestCase;
 
 class MoneyTest extends TestCase
 {
   public function testMultiplication(): void
   {
-    $dollor = new Doller(5);
-    $dollor->times(2);
+    $doller = new Doller(5);
+    $doller->times(2);
 
-    $this->assertSame(10, $dollor->amount);
+    $this->assertSame(10, $doller->amount);
   }
 }
