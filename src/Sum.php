6d2c3e1225eafa90e44d923e2283b07e5e78a8f1
TaneiKanta <hetaleotu@gmail.com>
2023-04-03 15:13:04 +0900

    fix: ExpressionInterfaceにメソッドを追加

--- src/Sum.php
+++ src/Sum.php
@@ -15,4 +15,9 @@ class Sum implements Expression
     $amount = $this->augend->reduce($bank, $to)->amount() + $this->addend->reduce($bank, $to)->amount();
     return new Money($amount, $to);
   }
+
+  public function plus(Expression $addend): Expression
+  {
+    // TODO: Implement plus() method.
+  }
 }
