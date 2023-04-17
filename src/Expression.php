3aa6731fcf3358229c47a5bc70082a3a43e71290
TaneiKanta <hetaleotu@gmail.com>
2023-04-17 14:44:34 +0900

    fix

--- src/Expression.php
+++ src/Expression.php
@@ -7,9 +7,5 @@ interface Expression
 {
   public function reduce(Bank $bank, string $to): Money;
   public function plus(Expression $addend): Expression;
-<<<<<<< HEAD
   public function times(int $multiplier): Expression;
-=======
-  public function times(int $multiplier): Expression
->>>>>>> main
 }
